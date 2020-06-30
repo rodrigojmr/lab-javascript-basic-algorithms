d6ecdfb775ae7b040955284046b81336ca5f5c2e
Rodrigo Moura <rodrigojorgemoura@gmail.com>
2020-06-30 18:17:20 +0100

    Fixed Bonus 2

--- js/index.js
+++ js/index.js
@@ -59,7 +59,7 @@ for (let i = hacker1.length - 1; i >= 0; i--) {
   if (hacker1[i] < hacker2[i]) {
     firstInOrder = hacker1;
     break;
-  } else if (hacker1[i] < hacker2[i]) {
+  } else if (hacker1[i] > hacker2[i]) {
     firstInOrder = hacker2;
     break;
   } else continue;
@@ -74,37 +74,36 @@ if (firstInOrder === hacker1) {
 }
 
 // Bonus 1
-const Lorem1 =
-  'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce non condimentum orci. Ut faucibus orci id interdum consequat. Ut velit enim, porttitor ac sollicitudin a, tempor vitae quam. Duis blandit in leo id lobortis. Morbi sit amet suscipit eros. In nibh augue, egestas a nunc consequat, aliquam maximus purus. Nullam venenatis commodo dolor fermentum convallis. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Cras dignissim, sem at posuere aliquet, elit justo vulputate eros, a sagittis nibh velit nec nisl. Integer quis purus elit.';
+const lorem = `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce non condimentum orci. Ut faucibus orci id interdum consequat. Ut velit enim, porttitor ac sollicitudin a, tempor vitae quam. Duis blandit in leo id lobortis. Morbi sit amet suscipit eros. In nibh augue, egestas a nunc consequat, aliquam maximus purus. Nullam venenatis commodo dolor fermentum convallis. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Cras dignissim, sem at posuere aliquet, elit justo vulputate eros, a sagittis nibh velit nec nisl. Integer quis purus elit. 
+  
+  Sed eu consectetur ipsum, non faucibus nibh. Morbi accumsan dolor in erat mollis pretium. Donec at justo odio. In sit amet lectus vitae est mattis malesuada non non dui. Nam gravida aliquet eros, eget cursus ex vehicula at. Vivamus malesuada dolor ac elementum condimentum. Nam tempus suscipit erat, eu maximus ex viverra vitae. Vivamus luctus velit enim, ut interdum odio ultricies et. Curabitur iaculis dolor sed ipsum tristique tempor. Vivamus molestie commodo hendrerit. Vivamus quam odio, accumsan at tellus vitae, bibendum posuere nunc. Mauris eget odio sit amet risus tempus consectetur et at dui. Pellentesque tempor odio at lacinia lacinia.
 
-const Lorem2 =
-  'Sed eu consectetur ipsum, non faucibus nibh. Morbi accumsan dolor in erat mollis pretium. Donec at justo odio. In sit amet lectus vitae est mattis malesuada non non dui. Nam gravida aliquet eros, eget cursus ex vehicula at. Vivamus malesuada dolor ac elementum condimentum. Nam tempus suscipit erat, eu maximus ex viverra vitae. Vivamus luctus velit enim, ut interdum odio ultricies et. Curabitur iaculis dolor sed ipsum tristique tempor. Vivamus molestie commodo hendrerit. Vivamus quam odio, accumsan at tellus vitae, bibendum posuere nunc. Mauris eget odio sit amet risus tempus consectetur et at dui. Pellentesque tempor odio at lacinia lacinia.';
-
-const Lorem3 =
-  'Nulla venenatis, metus vitae rhoncus tristique, libero orci convallis ligula, eu laoreet nulla tortor dignissim libero. Duis venenatis ac sapien quis molestie. Cras eget purus quis eros semper vestibulum ac at diam. Integer ac tortor sapien. Quisque vel congue sapien, at vehicula lorem. Nam vitae neque in justo consequat consectetur sit amet vitae neque. Fusce scelerisque arcu vitae cursus semper. Interdum et malesuada fames ac ante ipsum primis in faucibus. Fusce ut sodales tortor.';
+  Nulla venenatis, metus vitae rhoncus tristique, libero orci convallis ligula, eu laoreet nulla tortor dignissim libero. Duis venenatis ac sapien quis molestie. Cras eget purus quis eros semper vestibulum ac at diam. Integer ac tortor sapien. Quisque vel congue sapien, at vehicula lorem. Nam vitae neque in justo consequat consectetur sit amet vitae neque. Fusce scelerisque arcu vitae cursus semper. Interdum et malesuada fames ac ante ipsum primis in faucibus. Fusce ut sodales tortor.`;
 
 let totalWords = 0;
 
-for (let i = 0; i < Lorem1.length; i++) {
-  if (Lorem1[i] === ' ') {
-    totalWords += 1;
+if (lorem) {
+  for (let i = 0; i < lorem.length; i++) {
+    if (lorem[i] === ' ') {
+      totalWords += 1;
+    }
   }
+  totalWords += 1;
 }
-//  Account for last word
-totalWords += 1;
 
+//  Account for last word
 console.log(totalWords);
 
 // et Count
 
 let etCount = 0;
 
-for (let i = 0; i < Lorem1.length; i++) {
+for (let i = 0; i < lorem.length; i++) {
   if (
-    Lorem1[i] === ' ' &&
-    Lorem1[i + 1] === 'e' &&
-    Lorem1[i + 2] === 't' &&
-    Lorem1[i + 3] === ' '
+    lorem[i] === ' ' &&
+    lorem[i + 1] === 'e' &&
+    lorem[i + 2] === 't' &&
+    !(lorem[i + 3] >= 'a' && lorem[i + 3] <= 'Z')
   ) {
     etCount += 1;
   }
