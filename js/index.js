c39078695d53fbdd9d62ec9fecfd8779362837f5
Rodrigo Moura <rodrigojorgemoura@gmail.com>
2020-06-30 14:34:02 +0100

    iteration 1

--- js/index.js
+++ js/index.js
@@ -1,7 +1,12 @@
 // Iteration 1: Names and Input
+let hacker1 = 'John Doe';
 
+console.log(`The navigator's name is ${hacker1}`);
+
+let hacker2 = 'Navigator';
+
+console.log(`The navigator's name is ${hacker2}`);
 
 // Iteration 2: Conditionals
-
 
 // Iteration 3: Loops
